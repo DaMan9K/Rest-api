d0d5f0af5951e136b4fa96a24705589bb7458a23
A.Ostrovnoy <Zayacz.sit@gmail.com>
2022-03-07 18:30:50 +0300

    Initialise project

---
+++ cmd/main/app.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	//"context"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	"Rest-api/internal/config"
+	"Rest-api/internal/user"
+
+	//"Rest-api/internal/user/db"
+	//"Rest-api/pkg/client/mongodb"
+	"Rest-api/pkg/logging"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	logger := logging.GetLogger()
+	logger.Info("create router")
+	router := httprouter.New()
+
+	cfg := config.GetConfig()
+
+	// cfgMongo := cfg.MongoDB
+	// mongoDBClient, err := mongodb.NewClient(context.Background(), cfgMongo.Host, cfgMongo.Port, cfgMongo.Username, cfgMongo.Password, cfgMongo.Database, cfgMongo.AuthDB)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// storage := db.NewStorage(mongoDBClient, cfg.MongoDB.Collection, logger)
+
+	logger.Info("Register user handler")
+	handler := user.NewHandler(logger)
+	handler.Register(router)
+
+	start(router, cfg)
+
+}
+func start(router *httprouter.Router, cfg *config.Config) {
+	logger := logging.GetLogger()
+	logger.Info("start application")
+
+	var listener net.Listener
+	var listenErr error
+
+	if cfg.Listen.Type == "sock" {
+		logger.Info("detect app path")
+
+		appDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			logger.Fatal(err)
+		}
+		logger.Info("create socket")
+		socketPath := path.Join(appDir, "app.sock")
+
+		logger.Info("server is listen unix socket")
+		listener, listenErr = net.Listen("unix", socketPath)
+		logger.Debugf("server is listening unix socket %s:", socketPath)
+
+		// /path/to/binary
+		// Dir() -- /path/to
+	} else {
+		logger.Info("listen tcp")
+		listener, listenErr = net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+		logger.Infof("server is listening port %s:%s", cfg.Listen.BindIP, cfg.Listen.Port)
+
+	}
+	if listenErr != nil {
+		logger.Fatal(listenErr)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	logger.Fatal(server.Serve(listener))
+}
